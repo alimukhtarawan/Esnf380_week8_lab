7e37696ed23b86e8eed0c66cd00453ae077c83a2
JunyuHuo <junyu.huo@ucalgary.ca>
2025-03-03 11:25:16 -0700

    task completed

--- Task.java
+++ Task.java
@@ -4,6 +4,53 @@ import java.util.Objects;
 
 public class Task {
 
+
+
+
+    private String id;
+    private String title;
+    private boolean isCompleted;
+    
+    public Task(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = false;
+    }
+    
+    public Task(String id, String title, boolean isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = isCompleted;
+    }
+    public Task copy() {
+        return new Task(this.id, this.title, this.isCompleted);
+    }
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
